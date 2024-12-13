930b1a63718aaf9bd654fcbf7c68ac7988237a02
isha-patel-14 <isha.patel.cg@gmail.com>
2024-12-13 19:00:29 +0530

    about us page added

--- react-task/src/component/About.jsx
+++ react-task/src/component/About.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-      About Us Page
+    <div className="about-us-container">
+      <h1>About Us</h1>
+      <p>Welcome ! We are dedicated to bringing you the best of cuisine, refreshing mocktails, magical worlds, and innovative banking solutions. Our goal is to offer a unique experience where food lovers, Harry Potter fans, and curious minds can come together. Here's a little bit about what we do:</p>
+      
+      <section className="section">
+        <h2>Meals</h2>
+        <p>We celebrate the rich diversity of different types of food. Whether you're craving a spicy curry or a warm dessert, our collection of recipes covers the tastes of every regional food. Each recipe is carefully curated with traditional ingredients and cooking methods, offering a delightful experience for your taste buds.</p>
+      </section>
+
+      <section className="section">
+        <h2>Mocktails</h2>
+        <p>Looking for a refreshing drink to accompany your meal? Our mocktails section is packed with creative,alcohol-beverages, alcohol-free beverages, perfect for any occasion. From tangy fruit punches to creamy lassis, we ensure that every sip is as flavorful as our dishes.</p>
+      </section>
+
+      <section className="section">
+        <h2>Harry Potter</h2>
+        <p>Step into the magical world of Harry Potter! We've created an extensive collection of Harry Potter books, character profiles, spells, and more. Whether you're a Gryffindor or a Slytherin, our Harry Potter section is a treasure trove of information, featuring high-quality images and detailed content in various languages. Immerse yourself in the magic of the wizarding world!</p>
+      </section>
+
+      <section className="section">
+        <h2>Banking Solutions</h2>
+        <p>We understand the importance of banking data, which is why we created a this section to provide you with details about banks. 
+          We offer a simple solution to access useful banking information. Start exploring the data today and get insights into the world of banking!</p>
+      </section>
     </div>
   )
 }
--- react-task/src/component/react01.jsx
+++ react-task/src/component/react01.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const react01 = () => {
   return (
     <div>
-      WEBSITE-01
+      website1
     </div>
   )
 }
